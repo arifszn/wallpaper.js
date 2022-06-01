54c549cfb881a7225070b538e92446d1e52500d0
Ariful Alam <swazan.arif@gmail.com>
2022-06-01 20:06:21 +0600

    Scaffold project

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const { getRandomWallpaper } = require('./wallpaper');
+
+/**
+ * Get wallpaper.
+ *
+ * @param {object} obj
+ * @param {number} obj.total
+ * @param {boolean} obj.allowNSFW
+ * @param {boolean} obj.allowGIF
+ * @returns {array}
+ */
+const getWallpaper = async ({
+  total = 1,
+  allowNSFW = true,
+  allowGIF = true,
+} = {}) => {
+  try {
+    if (total > 20) {
+      throw Error('max value of total is 20');
+    } else if (total < 1) {
+      throw Error('min value of total is 1');
+    }
+
+    return await getRandomWallpaper({
+      total: parseInt(total),
+      allowNSFW: allowNSFW,
+      allowGIF: allowGIF,
+    });
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+
+module.exports.getWallpaper = getWallpaper;
